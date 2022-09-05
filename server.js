46602faccf3c8821da26bf202a4166c12cae58f2
sina shariati <sina.shariati@yahoo.com>
2022-09-05 13:58:09 +0430

    Add db.config.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+const {app} = require('./app');
 const env = require('dotenv');
 
 env.config();
---
+++ src/configs/db.config.js
@@ -0,0 +1,6 @@
+const db = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    document: process.env.DB_DOCUMENT
+};
+
