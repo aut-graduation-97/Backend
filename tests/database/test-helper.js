40b8c73255ad84986aa3813f8be33c256633b904
Ali Moradzade <alimorizz1379@gmail.com>
2022-09-06 13:55:50 +0430

    setting up mocha test env, and adding simple create user test

---
+++ tests/database/test-helper.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://localhost/aut_graduation_test`);
+
+before((done) => {
+    mongoose.connection
+        .once('open', () => {
+            done();
+        })
+        .on('error', (error) => {
+            console.warn('Warning', error);
+        });
+});
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        // Ready to run the next test!
+        done();
+    });
+});
