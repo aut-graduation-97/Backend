194cfbaccd6c25d9ffbe9304e03789b83752520a
Ali Moradzade <alimorizz1379@gmail.com>
2022-09-18 16:43:45 +0430

    doing some reformat, using changed db.bootstrap.js

--- src/db/db.bootstrap.js
+++ src/db/db.bootstrap.js
@@ -1,13 +1,16 @@
-const config = require('../configs/db.config');
+// const config = require('../configs/db.config');
 const mongoose = require('mongoose');
 
-const URL = `mongodb://${config.username}:${config.password}@${config.host}:${config.port}/${config.document}?${config.options}`;
+// const URL = `mongodb://${config.username}:${config.password}@${config.host}:${config.port}/${config.document}?${config.options}`;
+const URL = 'mongodb://localhost/aut_graduation';
 
-mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
+module.exports = () => {
+    // connect to db
+    if (process.env.NODE_ENV !== 'test') {
+        mongoose.connect('mongodb://localhost/aut_graduation')
+            .then(() => console.log('connected to db'))
+            .catch(err => console.error('could not connect to db', err));
+    }
+}
 
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-module.exports = db;
 
