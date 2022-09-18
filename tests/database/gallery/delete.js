441ae647a48b2a40f3d5a2e99a6a9983fdd227d2
Ali Moradzade <alimorizz1379@gmail.com>
2022-09-18 11:06:03 +0430

    adding db tests, almost ready to use

---
+++ tests/database/gallery/delete.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const Gallery = require('../../../src/db/models/gallery');
+
+describe('Deleting a gallery', () => {
+    let gallery;
+
+    beforeEach((done) => {
+        gallery = new Gallery({
+            images: [
+                "url1",
+                "url2",
+                "url3",
+                "url4",
+            ],
+        });
+
+        gallery.save()
+            .then(() => {
+                done();
+            });
+    });
+
+    it('removing a gallery', (done) => {
+        Gallery.deleteOne({_id: gallery._id})
+            .then(() => Gallery.findOne({images: gallery.images}))
+            .then((gallery) => {
+                assert(gallery === null);
+                done();
+            });
+    });
+});
