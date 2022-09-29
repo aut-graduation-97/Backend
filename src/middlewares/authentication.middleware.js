a977ae5f827a5265e81eb9b5e261741480696daf
Arefeh Kompani <awriik00@gmail.com>
2022-09-29 09:59:20 +0330

    Get user information by decoding the token

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -10,6 +10,35 @@ class auth {
         this.token = req.headers.authorization
         this.res = res
     }
+
+    getAllToken(){
+        return this.token
+    }
+
+    checkToken(){
+        return (this.getAllToken() && this.getAllToken().split(' ')[0] === 'Bearer') ? true : this.res.status(403).json({
+            message: "دسترسی غیر مجاز"
+        })
+        
+    }
+
+    getToken(){
+        return this.checkToken() ? this.getAllToken().split(' ')[1] : this.res.status(403).json({
+            message: "ورود شما نامعتبر است"
+        })
+    }
+
+    getUser(){
+        try {
+            var decoded = jwt.verify(this.getToken(), process.env.SECRET_KEY, {algorithm: "HS256"});
+            return decoded
+        }catch(e) {
+            console.log(e);
+            return this.res.status(403).json({
+                message: "زمان حضور شما منقضی شده است"
+            })
+        }
+    }
 }
 
 module.exports = auth
