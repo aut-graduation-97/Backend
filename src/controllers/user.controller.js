23ddf5cf4091864f9749f258cadcda1c838ef82f
sina shariati <sina.shariati@yahoo.com>
2022-10-11 13:32:49 +0330

    Change checking for auth middleware usage
    in the user.controller.js file there was the intention to check
    for the user being authenticated and then doing the rest of
    the logic which was done wrong, the getUser() function has to be called
    whcih returns the auth user.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,13 +3,11 @@ const authMiddleware = require('../middlewares/authentication.middleware');
 
 module.exports = {
     getAllUsers(req, res) {
-        const auth = new authMiddleware(req, res);
-
-        if (auth.checkToken()) {
-            res.json({
-                'message': 'لیست تمام کاربران با موفقیت دریافت شد',
-                'users': getAll(),
-            });
-        }
+        new authMiddleware(req, res).getUser();
+        
+        res.json({
+            'message': 'لیست تمام کاربران با موفقیت دریافت شد',
+            'users': getAll(),
+        });
     }
 };
