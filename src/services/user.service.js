9eadb699d3af5c2f819dfb0f309c6e41bb1a9cf0
sina shariati <sina.shariati@yahoo.com>
2022-10-24 22:10:13 +0330

    Add query for updating user data with studentId

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,8 @@
 const {getAll, getUserByStudentId} = require('../db/queries/user/read');
+const {updateUserWithStudentId} = require('../db/queries/user/update');
 const {getTweetsByUserId} = require('../db/queries/tweets/read');
+const {updateUserData} = require('../controllers/user.controller');
+
 module.exports = {
     async getAllUsers() {
         return await getAll();
@@ -8,5 +11,9 @@ module.exports = {
     async getTweetsByStudentId(studentId) {
         const user = await getUserByStudentId(studentId);
         return getTweetsByUserId(user._id);
+    },
+
+    async updateUserData(targetStudentId, newUserData) {
+        return updateUserWithStudentId(targetStudentId, newUserData);
     }
 };
